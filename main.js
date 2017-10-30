ba33f55014e79a96a15702999513ee81c23f98f1
Céleste Robinson <celestekrobinson@gmail.com>
2017-10-30 12:33:14 -0600

    Pressing keys on keyboard causes numbers to display

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 document.addEventListener('keydown', function(event) {
     if(event.keyCode === 49) {
-        
+        document.getElementById('one').click();
+    } else if (event.keyCode === 50) {
+        document.getElementById('two').click();
+    } else if (event.keyCode === 51) {
+        document.getElementById('three').click();
+    } else if (event.keyCode === 52) {
+        document.getElementById('four').click();
+    } else if (event.keyCode === 53) {
+        document.getElementById('five').click();
+    } else if (event.keyCode === 54) {
+        document.getElementById('six').click();
+    } else if (event.keyCode === 55) {
+        document.getElementById('seven').click();
+    } else if (event.keyCode === 56) {
+        document.getElementById('eight').click();
+    } else if (event.keyCode === 57) {
+        document.getElementById('nine').click();
+    } else if (event.keyCode === 48) {
+        document.getElementById('zero').click();
+    } else if (event.keyCode === 190.) {
+        document.getElementById('decimal').click();
+    } else if (event.keyCode === 189) {
+        document.getElementById('subtract').click();
+    } else if (event.keyCode === 191) {
+        document.getElementById('divide').click();
+    } else if (event.keyCode === 56) {
+        document.getElementById('multiply').click();
+    } else if (event.keyCode === 187) {
+        document.getElementById('add').click();
     }
 });
 
